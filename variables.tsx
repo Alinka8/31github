49d3515ab0d0c2e103a831f26805a958b98379e9
Alinka <alinazoldubaeva@gmail.com>
2023-11-22 17:38:52 -0500

    variables

---
+++ variables.tsx
@@ -0,0 +1,7 @@
+var firstname: string = "John";
+var score1: number = 50;
+var score2: number = 42.5;
+var sum = score1 + score2;
+console.log("name" + firstname);
+console.log("second score: " + score2);
+console.log("sum of the scores: " + sum);
