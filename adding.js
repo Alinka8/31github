2798d1640b3bb73010feaa8945d0a86f457317b0
Alinka <alinazoldubaeva@gmail.com>
2023-12-05 22:53:05 -0500

    adding

---
+++ adding.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function adding() {
+  if (str1 === null || str1 === undefined || str1.substring(0, 2) === "Py") {
+    return str1;
+  }
+  return "Py" + str1;
+}
+
+console.log(string_check("Python"));
+console.log(string_check("thon"));
