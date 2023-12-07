62b3cb67a2738029898bd5523cfb82ae0067f8f0
Alinka <alinazoldubaeva@gmail.com>
2023-12-06 23:08:00 -0500

    ternary

---
+++ ternaty.js
@@ -0,0 +1,7 @@
+function Goal(props) {
+  const isGoal = props.isGoal;
+  return <>{isGoal ? <MadeGoal /> : <MissedGoal />}</>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Goal isGoal={false} />);
